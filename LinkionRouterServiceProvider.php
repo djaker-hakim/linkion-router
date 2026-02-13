f54551cb90258506b5fac4ec14bf4f041441f895
djaker hakim <hakiltheknight@gmail.com>
2026-02-13 19:38:22 +0100

    first commit

---
+++ LinkionRouterServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Linkion\Router;
+
+use Illuminate\Support\ServiceProvider;
+
+class LinkionRouterServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
