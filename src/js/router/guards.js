2acd12dd9531fcfe0a26f5783af27d0a5240c516
djaker hakim <hakiltheknight@gmail.com>
2026-03-18 11:00:47 +0100

    adding a remove system to guards

--- src/js/router/guards.js
+++ src/js/router/guards.js
@@ -4,24 +4,30 @@ export const guards = {
     guards: {},
 
     addGuard(event, callback) {
-        if(!this.guards[event]) this.guards[event] = [];
-        this.guards[event].push(callback);
+        if(!this.guards[event]) this.guards[event] = new Map();
+        const id = crypto.randomUUID();
+        this.guards[event].set(id, callback);
+        return id;
     },
-
-    // TODO remove guard
     
+    // remove guard
+    removeGuard(id){
+        for(const event of Object.keys(this.guards)){
+            if(this.guards[event].has(id)){
+                this.guards[event].delete(id);
+                return true;
+            }
+        }
+        return false;
+    },
 
     runGuards(event, to, from) {
         for (const guard of this.guards[event] || []) {
             const result = this.executeGuard(guard, to, from);
             
             // If guard returns false or redirect, stop execution
-            if (result !== true) {
-                // TODO add navigate cancel event
-                return result;
-            }
+            if (result !== true) return result;
         }
-        
         return true; // All guards passed
     },
 
